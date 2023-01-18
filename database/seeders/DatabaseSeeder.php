a98bb683086f0b981886fcf98604c2f5ce884b70
SR Tusher <sr.tushar.pro@gmail.com>
2023-01-18 23:30:25 +0600

    facker added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $faker = Faker::create();
+        foreach (range(1, 30) as $index) {
+            $user = new User();
+            $user->name = $faker->name;
+            $user->email = $faker->email;
+            $user->password = Hash::make('12345678');
+            $user->save();
+        }
+
     }
 }
